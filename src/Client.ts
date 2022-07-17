65c120f0777df6a92418eaa1ece15ad8daaf311b
zaida04 <nico.03727+github@gmail.com>
2022-07-16 23:11:19 -0400

    chore: set diff port for redis

--- src/Client.ts
+++ src/Client.ts
@@ -2,5 +2,5 @@ import { BotClient } from "@guildedjs/gil";
 import RedisClient from "ioredis";
 
 export class StarboardClient extends BotClient {
-	public readonly redis = new RedisClient(process.env.REDIS_URL ?? "cache:6379");
+	public readonly redis = new RedisClient(process.env.REDIS_URL ?? "cache:6380");
 }
